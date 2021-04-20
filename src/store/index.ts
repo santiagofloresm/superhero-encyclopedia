a03cc2da9edef31f0f0927bf8fa8bf2c9a22770c
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2021-04-19 19:12:07 -0600

    Created store module for superheros

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import superheros from './Modules/superheros.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    superheros
   }
 })
