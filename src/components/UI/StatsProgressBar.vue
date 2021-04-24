f2937faab02c78086b89481946a9aaa20edea9e8
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2021-04-24 14:32:23 -0600

    Added stats bar

---
+++ src/components/UI/StatsProgressBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <span class="superhero-stats">{{ name }}
+    <v-progress-linear
+        class="superhero-progress"
+        :color="color"
+        height="13"
+        :value="value"
+        striped></v-progress-linear>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'StatsProgressBar',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Number,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true,
+    }
+  }
+})
+</script>
+
+<style>
+.superhero-stats {
+  font-size: 27px;
+  font-weight: bold;
+  color: white;
+}
+
+.superhero-progress {
+  /*width: 50px;*/
+}
+</style>
